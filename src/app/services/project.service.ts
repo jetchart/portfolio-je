88764abf2ddd20036103139661f57b8b488ad1c8
Juan M. Etchart <juanmetchart@gmail.com>
2019-06-22 18:35:26 -0300

    Add scrolleable class on modal

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -14,7 +14,7 @@ export class ProjectService {
     let project = new Project();
     project.id = 0;
     project.title = "First project";
-    project.content = "<b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam. ";
+    project.content = "<b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam. <b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam. <b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam. <b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam. <b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam. ";
     project.image = "assets/cabin.png";
     projects.push(project);
 
